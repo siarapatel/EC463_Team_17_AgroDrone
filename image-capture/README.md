7c93d759aeb3a7ab5d112b07a02877eb47d36e70
Luke McCarthy <mail@lukehmcc.com>
2025-11-16 18:10:31 -0500

    image-capture: add gpio watcher

--- image-capture/README.md
+++ image-capture/README.md
@@ -4,3 +4,6 @@
 
 To run the project, do `uv run main.py`. This inits the venv correctly so all
 packages are installed for you.
+
+## Internal
+We are going to measure pin 27 (BCM)
--- image-capture/main.py
+++ image-capture/main.py
@@ -1,9 +1,24 @@
+import RPi.GPIO as GPIO
+import time
 from dual_sequential_capture import dual_caputure
 
 
+pin = 27  # BCM Pin
+
+
+# on a loop, check the gpio state
 def main():
     print("Hello from image-capture!")
-    dual_caputure()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # Start watching for the gpio value
+    while True:
+        state = GPIO.input(pin)  # returns 0 (low) or 1 (high)
+        if state:
+            dual_caputure
+            time.sleep(5)  # dumb sleep to not do duplicates
+        time.sleep(0.1)
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
